0f57eea8bc4ddcfcdd60f69c6ea26ecc83974cdd
onEihou <on@safie.jp>
2019-02-26 12:11:36 +0900

    first

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  srcDir: 'src',
+  head: {
+    titleTemplate: '%s | NUXT FIREBASE SSR sample'
+  },
+  env: {
+    APIKEY: 'AIzaSyB3GVfqr7Yf4Q_b1KdFBDFgocqegGpZjHc',
+    AUTHDOMAIN: 'fir-nuxt-wordpress.firebaseapp.com',
+    DATABASEURL: 'https://fir-nuxt-wordpress.firebaseio.com',
+    PROJECTID: 'fir-nuxt-wordpress',
+    STORAGEBUCKET: 'fir-nuxt-wordpress.appspot.com',
+    MESSAGINGSENDERID: '221920909823'
+  }
+}
