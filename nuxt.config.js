4d6e8edbc0eefb326e5bb978661430494253588c
onEihou <on@safie.jp>
2019-02-26 16:38:14 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,11 @@
 module.exports = {
+  mode: 'universal',
   srcDir: 'src',
   head: {
     titleTemplate: '%s | NUXT FIREBASE SSR sample'
   },
   env: {
-    APIKEY: 'AIzaSyB3GVfqr7Yf4Q_b1KdFBDFgocqegGpZjHc',
-    AUTHDOMAIN: 'fir-nuxt-wordpress.firebaseapp.com',
-    DATABASEURL: 'https://fir-nuxt-wordpress.firebaseio.com',
-    PROJECTID: 'fir-nuxt-wordpress',
-    STORAGEBUCKET: 'fir-nuxt-wordpress.appspot.com',
-    MESSAGINGSENDERID: '221920909823',
     HOST: 'http://52.193.48.36'
-    // HOST: 'http://www.renowan.com/blog'
   },
   vendor: ['axios'],
   css: [
